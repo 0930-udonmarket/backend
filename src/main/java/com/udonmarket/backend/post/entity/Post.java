a741fd9772fc3cbe8f681827dd8c59c464cb9d57
Eunjin-n <ikee1652@duksung.ac.kr>
2026-03-20 03:10:03 +0900

    feat: Post 수정

--- src/main/java/com/udonmarket/backend/post/entity/Post.java
+++ src/main/java/com/udonmarket/backend/post/entity/Post.java
@@ -1,5 +1,7 @@
 package com.udonmarket.backend.post.entity;
 
+import com.udonmarket.backend.product.entity.Product;
+import com.udonmarket.backend.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,9 +18,22 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // 판매자 정보
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     private String title;
     private String content;
+    private Long targetCount; // 모집 인원
+    private Long currentCount; // 현재 모집 인원
     private String status;
+    private Long pricePerPerson; // 1인당 부담금
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
     @CreatedDate
     @Column(updatable = false)
