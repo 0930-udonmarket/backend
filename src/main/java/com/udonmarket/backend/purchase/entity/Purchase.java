e86f5befe683cbc42499e677f6e10108d4682d14
Eunjin-n <ikee1652@duksung.ac.kr>
2026-03-20 03:12:30 +0900

    Purchase 수정

--- src/main/java/com/udonmarket/backend/purchase/entity/Purchase.java
+++ src/main/java/com/udonmarket/backend/purchase/entity/Purchase.java
@@ -1,4 +1,23 @@
 package com.udonmarket.backend.purchase.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
 public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long requestedAmount; // 결제 시도 금액 - 전체 금액
+    private Long approvedAmount; // 승인 금액 - 1인당 부담 금액
+
 }
