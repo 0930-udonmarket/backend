5d85e0499266c6e8fb9e43a18c4960f3cc117808
comeonunited <manchesterfan@naver.com>
2026-03-27 13:55:43 +0900

    remove: udonmarket 백엔드 파일 마지막 삭제

--- src/main/java/com/udonmarket/backend/review/repository/ReviewImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udonmarket.backend.review.repository;
-
-import com.udonmarket.backend.review.entity.ReviewImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
-}
